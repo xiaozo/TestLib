afe3c2b74596485f3c53f58a3a5f8f337bd44451
DeerClass <deerclass@DeerClassdeMac-mini.local>
2022-04-12 11:19:37 +0800

    1

---
+++ TestLib/TestLib/TestPublicLib.h
@@ -0,0 +1,16 @@
+//
+//  TestPublicLib.h
+//  TestLib
+//
+//  Created by DeerClass on 2022/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestPublicLib : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestLib/TestLib/TestPublicLib.m
@@ -0,0 +1,14 @@
+//
+//  TestPublicLib.m
+//  TestLib
+//
+//  Created by DeerClass on 2022/4/12.
+//
+
+#import "TestPublicLib.h"
+
+@implementation TestPublicLib
+-(NSString *)description {
+    return @"user TestLib";
+}
+@end
